9445160b986b8e75818199558fd3430476cdda57
Alina Ofenheimer <a.ofenheimer@gmx.net>
2021-11-08 23:44:20 +0100

    now can also compute zscores with more advanced formulas

--- scientific_zscore_app.py
+++ scientific_zscore_app.py
@@ -16,13 +16,8 @@ child_adult_split = 18.0
 max_age = 82.0
 male = 0
 female = 1
-path = 'data/'
-age_col = 'age'
-gender_col = 'gender'
 filename_children = 'children_LMS_{}_gender{}.csv'
 filename_adults = 'adults_LMS_{}_gender{}.csv'
-
-
 
 #%% FUNCTION DEFINITIONS
 def compute_zscore(y_vals, t_vals, L, M, S, eps=0.00001):
@@ -32,7 +27,8 @@ def compute_zscore(y_vals, t_vals, L, M, S, eps=0.00001):
     zscores = np.empty_like(y_vals)
     small = abs(l_vals) < eps
     zscores[small] = np.log(y_vals[small]/m_vals[small]) / s_vals[small]
-    zscores[~small] = ((y_vals[~small]/m_vals[~small])**l_vals[~small] - 1)/(l_vals[~small]*s_vals[~small])
+    zscores[~small] = ((y_vals[~small]/m_vals[~small])**l_vals[
+        ~small] - 1)/(l_vals[~small]*s_vals[~small])
 
     # zscores = []
     # for y, l, m, s in zip(y_vals, l_vals, m_vals, s_vals):
@@ -44,23 +40,71 @@ def compute_zscore(y_vals, t_vals, L, M, S, eps=0.00001):
     return zscores
 
 
-def get_LMS(param, gender, filename, path=path):
+def compute_zscore_splines(
+        y_vals, t_vals, h_vals, L, M, S, Lspline, Mspline, Sspline, eps=0.00001
+):
+    l_vals = L(t_vals, h_vals, Lspline(t_vals))
+    m_vals = M(t_vals, h_vals, Mspline(t_vals))
+    s_vals = S(t_vals, h_vals, Sspline(t_vals))
+    if type(s_vals) == float:
+        s_vals = np.ones_like(m_vals) * s_vals
+    if type(l_vals) == float:
+        l_vals = np.ones_like(m_vals) * l_vals
+    zscores = np.empty_like(y_vals)
+    small = abs(l_vals) < eps
+    zscores[small] = np.log(y_vals[small] / m_vals[small]) / s_vals[small]
+    zscores[~small] = ((y_vals[~small] / m_vals[~small]) ** l_vals[
+        ~small] - 1) / (l_vals[~small] * s_vals[~small])
+
+    return zscores
+
+
+def get_formula(s):
+    s = s.strip().lower().replace("exp", "np.exp").replace("log", "np.log").replace(
+        "ln", "np.log").replace("mspline", "spline").replace(
+        "sspline", "spline").replace("lspline", "spline")
+    f = eval("lambda age, height, spline: {}".format(s))
+    return f
+
+
+def get_LMS(param, gender, filename, path):
     file_path = os.path.join(path, filename.format(param, gender))
-    df_ref = pd.read_csv(file_path)
-    L = interp.interp1d(x=df_ref['age'], y=df_ref['lambda'], kind='cubic')
-    M = interp.interp1d(x=df_ref['age'], y=df_ref['mu'], kind='cubic')
-    S = interp.interp1d(x=df_ref['age'], y=df_ref['sigma'], kind='cubic')
-    return L, M, S
+    try:
+        df_ref = pd.read_csv(file_path)
+    except Exception:
+        df_ref = pd.read_excel(file_path.replace(".csv", ".xlsx"))
+
+    # distinguish whether LMS params are given or need to be computed
+    cols = df_ref.columns
+    if "lambda" in cols and "mu" in cols and "sigma" in cols:
+        L = interp.interp1d(x=df_ref['age'], y=df_ref['lambda'], kind='cubic')
+        M = interp.interp1d(x=df_ref['age'], y=df_ref['mu'], kind='cubic')
+        S = interp.interp1d(x=df_ref['age'], y=df_ref['sigma'], kind='cubic')
+        return L, M, S, None, None, None
+    elif "Mspline" in cols and "Sspline" in cols and "Lspline" in cols:
+        M_formula = get_formula(str(df_ref["M"].values[0]))
+        S_formula = get_formula(str(df_ref["S"].values[0]))
+        L_formula = get_formula(str(df_ref["L"].values[0]))
+        Lspline = interp.interp1d(
+            x=df_ref['age'], y=df_ref['Lspline'], kind='cubic')
+        Mspline = interp.interp1d(
+            x=df_ref['age'], y=df_ref['Mspline'], kind='cubic')
+        Sspline = interp.interp1d(
+            x=df_ref['age'], y=df_ref['Sspline'], kind='cubic')
+        return L_formula, M_formula, S_formula, Lspline, Mspline, Sspline
 
 
-def compute_zscores_file(filename):
+def compute_zscores_file(filename, datapath, age_col, gender_col, height_col):
     df = pd.read_excel(filename, header=0, index_col=None)
 
     assert age_col in df.columns and gender_col in df.columns, \
         "file does not have the columns: {} and {}".format(age_col, gender_col)
+    height_col_exist = True
+    if height_col not in df.columns:
+        height_col_exist = False
 
     for col in df.columns:
-        if col not in [age_col, gender_col, 'ID']:
+        if col not in [age_col, gender_col, height_col, 'ID', "PT_Nr"]:
             print('computing zscores for: {}'.format(col))
             new_col = 'zscore-{}'.format(col)
             for gender in [male, female]:
@@ -77,10 +121,25 @@ def compute_zscores_file(filename):
                                 file_name = filename_adults
                             y_vals = df_curr[col].values
                             t_vals = df_curr[age_col].values
-                            L, M, S = get_LMS(
-                                param=col, gender=gender, filename=file_name)
-                            zscores = compute_zscore(y_vals, t_vals, L, M, S)
+                            if height_col_exist:
+                                h_vals = df_curr[height_col].values
+                            else:
+                                h_vals = np.zeros_like(t_vals)
+                            L, M, S, Lspline, Mspline, Sspline = get_LMS(
+                                param=col, gender=gender, filename=file_name,
+                                path=datapath)
+                            if Lspline is None and Sspline is None \
+                                    and Mspline is None:
+                                zscores = compute_zscore(y_vals, t_vals, L, M, S)
+                            else:
+                                if not height_col_exist:
+                                    print("WARNING: height column not found, "
+                                          "using 0s instead")
+                                zscores = compute_zscore_splines(
+                                    y_vals, t_vals, h_vals, L, M, S,
+                                    Lspline, Mspline, Sspline)
                         except Exception:
+                            # raise
                             zscores = None
                         df.loc[
                             (df[age_col] >= age_int[0]) &
@@ -98,10 +157,32 @@ if __name__ == '__main__':
         help="filename (with path if not in same directory), "
              "default: 'example_file.xlsx'",
         default="example_file.xlsx")
+    parser.add_argument(
+        '--datapath', type=str,
+        help="path to data",
+        default="data/")
+    parser.add_argument(
+        '--age', type=str,
+        help="name of column with age values",
+        default="age")
+    parser.add_argument(
+        '--gender', type=str,
+        help="name of column with gender values",
+        default="gender")
+    parser.add_argument(
+        '--height', type=str,
+        help="name of column with height values",
+        default="height")
 
     args = parser.parse_args()
     filename = args.filename
-    compute_zscores_file(filename)
+    datapath = args.datapath
+    age_col = args.age
+    gender_col = args.gender
+    height_col = args.height
+    compute_zscores_file(
+        filename=filename, datapath=datapath, age_col=age_col,
+        gender_col=gender_col, height_col=height_col)
 
 
 
